eb507f9f1f76f1fc43f46891dca591a686d634b7
Dal Robinson <dalwrobinson@protonmail.com>
2022-11-29 11:36:11 +0000

    pundit error fix.  Validations removed from models

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i[index show] do
+    resources :playlists
+  end
 end

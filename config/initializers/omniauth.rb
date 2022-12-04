fe12eefa4a1fd94664c20c736aad95810a4cfa0e
Dal Robinson <dalwrobinson@protonmail.com>
2022-12-04 12:00:52 +0000

    omniauth.rb file removed - not needed

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-require 'rspotify/oauth'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV.fetch('CLIENT_ID'), ENV.fetch('CLIENT_SECRET'), scope: 'ugc-image-upload playlist-read-private playlist-modify-private playlist-modify-public'
-end
-
-OmniAuth.config.allowed_request_methods = [:post, :get]

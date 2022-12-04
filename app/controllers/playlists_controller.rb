5f75f2b13da1c2ba28792910d5d38c5ea3468809
Dal Robinson <dalwrobinson@protonmail.com>
2022-12-04 12:09:08 +0000

    spotify scopes expanded

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -51,7 +51,7 @@ class PlaylistsController < ApplicationController
         client_id: ENV['CLIENT_ID'],
         response_type: "code",
         redirect_uri: "http://localhost:3000/playlists/",
-        scope: "user-library-read ugc-image-upload playlist-read-private playlist-modify-private playlist-modify-public",
+        scope: "user-library-read ugc-image-upload playlist-read-private playlist-modify-private playlist-modify-public user-read-private user-top-read user-follow-read",
         show_dialog: true
       }
       @authorize_spotify_link = "https://accounts.spotify.com/authorize?#{query_params.to_query}"

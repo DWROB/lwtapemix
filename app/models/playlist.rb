099906b10fb9d524c63ebe86f1491c3c925a4900
ricardomendes <ricardogaditmendes26@gmail.com>
2022-12-06 14:09:25 +0000

    made plalist name unique

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,6 @@ class Playlist < ApplicationRecord
   belongs_to :user
   has_many :songs, dependent: :destroy
   has_many :votes, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end

a07ee5f3f21eb1256cb8de751d60cf64b03c0f3c
Dal Robinson <dalwrobinson@protonmail.com>
2022-12-06 17:07:30 +0000

    mini fix on the QR code

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,7 +11,7 @@ class VotesController < ApplicationController
     authorize @songs_votes
     skip_policy_scope
     authorize @playlist
-    @qr_code = RQRCode::QRCode.new(playlist_url(@playlist))
+    @qr_code = RQRCode::QRCode.new("lwtapemix.herokuapp.com/playlists/#{@playlist.id}/welcome/")
     @svg = @qr_code.as_svg(
       offset: 0,
       color: 'fff',

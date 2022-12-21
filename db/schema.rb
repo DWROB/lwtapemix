27abf8eb90a839ba3be2652b9833584e6ee65326
Dal Robinson <dalwrobinson@protonmail.com>
2022-12-21 17:57:54 +0000

    removed role from users as have added admin=bool for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_134549) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_175252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,7 +76,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_134549) do
     t.string "name"
     t.string "spotify_name"
     t.string "spotify_access_token"
-    t.integer "role", default: 0
     t.string "refresh_token"
     t.text "auth_key"
     t.boolean "admin", default: false, null: false

9079e548b295b1c9fbf8034dc35a5ff1f0e50c45
Dal Robinson <dalwrobinson@protonmail.com>
2023-01-02 09:39:28 +0000

    clean: remove api folder.  Fix: line spacing and setting some oneline if statements

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -122,9 +122,7 @@ class PlaylistsController < ApplicationController
     @playlist = Playlist.find(params[:playlist_id])
 
     @playlist.songs.each do |song|
-      if song.votes.empty? || song.votes[0].votes.negative?
-        Song.delete(song.id)
-      end
+      Song.delete(song.id) if song.votes.empty? || song.votes[0].votes.negative?
     end
 
     @user = User.find(@playlist.user_id)
